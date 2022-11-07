32eb5db63faff7f44747dc6452951b6c8e2ac4b9
Abdullah Bakış <abdullahbakis1672@gmail.com>
2022-11-07 16:13:30 +0300

    Update

--- src/main/java/com/abdullah/springbootimagegallery/repository/ImageGalleryRepository.java
+++ src/main/java/com/abdullah/springbootimagegallery/repository/ImageGalleryRepository.java
@@ -1,2 +1,10 @@
-package com.abdullah.springbootimagegallery.repository;public class ImageGalleryRepository {
+package com.abdullah.springbootimagegallery.repository;
+
+import com.abdullah.springbootimagegallery.entity.ImageGallery;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ImageGalleryRepository extends JpaRepository<ImageGallery, Long> {
+
 }
